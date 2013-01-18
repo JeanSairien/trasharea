f59dc001bfc4be3bd2d52eba3e5ef39fd39cd67a
niamtokik <niamkik@gmail.com>
2013-01-18 08:48:09 +0100

    adding copyright & complete some functions

--- shell/pkgng/_pkg.bash
+++ shell/pkgng/_pkg.bash
@@ -1,3 +1,42 @@
+#! /bin/bash
+######################################################################
+#                                                                    #
+# Copyright (c) 2013, Niamkik <niamkik@gmail.com>                    # 
+# All rights reserved.                                               #
+#                                                                    #
+# Redistribution and use in source and binary forms, with or without #
+# modification, are permitted provided that the following conditions #
+# are met:                                                           #
+#                                                                    #
+# 1. Redistributions of source code must retain the above copyright  #
+#    notice, this list of conditions and the following disclaimer.   #
+#                                                                    #
+# 2. Redistributions in binary form must reproduce the above         #
+#    copyright notice, this list of conditions and the following     #
+#    disclaimer in the documentation and/or other materials provided #
+#    with the distribution.                                          #
+#                                                                    #
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND             #
+# CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES,        #
+# INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF           #
+# MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE           #
+# DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS  #
+# BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,           #
+# EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED    #
+# TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,      #
+# DATA, OR PROFITS; OR BUSINESS INTERRUPTION)HOWEVER CAUSED AND ON   #
+# ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR #
+# TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF #
+# THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF    #
+# SUCH DAMAGE.                                                       #
+#                                                                    #
+# The views and conclusions contained in the software and            #
+# documentation are those of the authors and should not be           #
+# interpreted as representing official policies, either expressed or #
+# implied, of the FreeBSD Project.                                   #
+#                                                                    #
+######################################################################
+
 _pkg_installed () {
     pkg query "%n-%v"
 }
@@ -75,19 +114,45 @@ _pkg () {
 		return 0 ;;
 
         backup) 
-	    COMPREPLY=() && \
-		return 0 ;;
+	    COMPREPLY=(
+		'(-r)-d[Dumps the local package database]'
+		'(-d)-r[Restrore the local package database]'
+	    )
+	    return 0 
+	    ;;
 
         check) 
-	    COMPREPLY=() && \
-		return 0 ;;
+	    COMPREPLY=(
+		'-B[reanalyse SHLIBS of installed packages]' 
+		'-d[check for and install missing dependencies]' 
+		'-r[recompute sizes and checksums of installed]' 
+		'-s[find invalid checksums]' 
+		'-v[Be verbose]' 
+		'(-g -x -X)-a[Process all packages]' 
+		'(-x -X -a)-g[Process packages that matches glob]'
+		'(-g -X -a)-x[Process packages that matches regex]'
+		'(-g -x -a)-X[Process packages that matches extended regex]'
+	    )
+	    return 0 
+	    ;;
 
         clean) 
 	    return 0 ;;
 	
         create) 
-	    COMPREPLY=() && \
-		return 0 ;;
+	    COMPREPLY=(
+		'-r[Root directory] -/'
+		'-m[Manifest directory] -/'
+		'-f[format]'
+		'-o[Ouput directory] -/'
+		'(-g -x -X)-a[Process all packages]'
+		'(-x -X -a)-g[Process packages that matches glob]'
+		'(-g -X -a)-x[Process packages that matches regex]'
+		'(-g -x -a)-X[Process packages that matches extended regex]'
+		'*:Package:_pkg_installed'
+	    )
+	    return 0 
+	    ;;
 
         delete|remove) 
 	    COMPREPLY=() && \
