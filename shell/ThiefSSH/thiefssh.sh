58117d4abb7d485a91fa8fb5866577cfbc1e69a8
niamtokik <niamkik@gmail.com>
2013-08-21 22:16:46 +0200

    adding some small modification, not functionnaly yet...

--- shell/ThiefSSH/thiefssh.sh
+++ shell/ThiefSSH/thiefssh.sh
@@ -1,4 +1,4 @@
-#! /bin/sh
+#! /usr/bin/env sh
 ######################################################################
 # very small script for remote control access. 
 #
@@ -24,44 +24,117 @@
 # DISCLAIMED. IN NO EVENT SHALL <COPYRIGHT HOLDER> BE LIABLE FOR
 # ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
 # CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
-# PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA,                                                                                
-# OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY                                                                                
-# THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR
+# PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA,                                                                               # OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY                                                                               # THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR
 # TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT
 # OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY
 # OF SUCH DAMAGE.
+#
+# Note: please set default variables, script is not ready to work
+#       only with the prompt.
 ######################################################################
 
+# default variables
 TARGET_HOST=""
 TARGET_PORT=""
 TARGET_USER=""
 TARGET_PKEY=""
-SSH_FORWARD="-R 12345:localhost:22"
+
+# ssh specific variables
+SSH_FORWARD=("-R 127.0.0.1:12345:127.0.0.1:22"
+             "-D 127.0.0.1:9999")
 SSH_OPTIONS="-i $TARGET_PKEY -p $TARGET_PORT -C -N $SSH_FORWARD"
-SSH_LAUNCH="ssh $TARGET_USER@$TARGET_HOST $SSH_OPTIONS"
+SSH_EXTOPTS="-o Compression=yes -o CompressionLevel=9"
+SSH_LAUNCH="ssh $TARGET_USER@$TARGET_HOST $SSH_OPTIONS $SSH_EXTOPTS"
 
+# command variables may change on some systems
+CMD_PING=$(which ping 2>/dev/null || print "/bin/ping")
+CMD_NC=$(which nc 2>/dev/null || print "/usr/bin/nc")
+CMD_PGREP=$(which pgrep 2>/dev/null || print "/usr/bin/pgrep")
+CMD_PRINTF=$(which printf 2>/dev/null || print "/usr/bin/printf")
+
+# default flags
+FLAG_DEBUG="yes"
+FLAG_PING=""
+FLAG_NC=""
+
+ICMP_HOST="8.8.8.8"
+
+# define default print
 print () {
-    printf "$*\n"
+    printf -- "$*\n"
 }
 
-check_icmp () {
-    ping -c 1 "$1" 2>&1 > /dev/null
+# define debug print
+print_d () {
+    print "debug: $*"
 }
 
-check_tcp () {
-    nc -z -p "$2" "$1" 2>&1 > /dev/null
+usage () {
+    print "usage: $0 set [target_host|target_port|target_user|target_pkey]"
+    print "       $0 usage"
+    print "       $0 check"
 }
 
-ssh_thief () {
-    if ! pgrep -f "$SSH_LAUNCH" 2>&1 > /dev/null
+# check all softs
+check_soft () {
+    if [ -x "${CMD_PING}" ]
     then
-        $SSH_LAUNCH &
+	FLAG_PING="yes"
+    fi
+
+    if [ -x "${CMD_NC}" ]
+    then
+	FLAG_NC="yes"
     fi
 }
 
-if (check_icmp 8.8.8.8) || \
-   (check_tcp $TARGET_HOST $TARGET_PORT)
-then
-   ssh_thief
-fi
+# check variables
+check_variables () {
+    # check command variables
+    for cmd in $(set | egrep "^CMD_.*")
+    do
+	if [ -x "${cmd##*=}" ]
+	then
+	    print_d "${cmd%%=*} => ${cmd##*=} is ok"
+	fi
+    done
+}
 
+# check icmp status (not very important)
+check_icmp () {
+    ${CMD_PING} -c 1 "$1" 2>&1 > /dev/null
+}
+
+# check tcp port status
+check_tcp () {
+    ${CMD_NC} -z -p "$2" "$1" 2>&1 > /dev/null
+}
+
+# init ssh thief command
+ssh_thief () {
+    if ! ${CMD_PGREP} -f "$SSH_LAUNCH" 2>&1 > /dev/null
+    then
+        ${SSH_LAUNCH} &
+    fi
+}
+
+# make all data test with flag_debug
+#if [ "$FLAG_DEBUG" ]
+#then
+#    check_variables
+#fi
+
+if [ "$1" ]
+then
+    case "$1" in
+	"set") print "work in progress" ;;
+	"check") print "work in progress" ;;
+	*) usage ;;
+    esac
+else
+    # check ICMP or TCP status
+    if (check_icmp ${ICMP_HOST}) || (check_tcp ${TARGET_HOST} ${TARGET_PORT})
+    then
+	ssh_thief
+    fi
+fi
