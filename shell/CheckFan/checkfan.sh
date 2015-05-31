105a1d20ccf598632226fd3cb71a06a04f56ef18
dem <dem@localhost>
2015-05-31 13:46:20 +0200

    - added more detailed help  - added loop support  - added auto-search thermal information function  - added more comments.

--- shell/CheckFan/checkfan.sh
+++ shell/CheckFan/checkfan.sh
@@ -1,6 +1,6 @@
 #! /bin/sh
 ######################################################################
-# Copyright (c) 2015, niamkik <niamkik@gmail.com>                    #
+# Copyright (c) 2015, niamkik <niamkik@gmail.com>                    #
 # All rights reserved.                                               #
 #                                                                    #
 # Redistribution and use in source and binary forms, with or         #
@@ -37,15 +37,24 @@ FAN_LEVEL="7"
 COUNTER="1"
 SLEEP="10"
 DEBUG=""
+SENSORS="hw.acpi.thermal.tz0.temperature
+         hw.acpi.thermal.tz1.temperature
+         dev.cpu.0.temperature
+         dev.cpu.1.temperature"
+
+THERMAL_KEYS="hw\.acpi\.thermal\.tz[0-9]+\.temperature
+              dev\.cpu\.[0-9]+.temperature"
 
 ######################################################################
 # global functions                                                   #
 ######################################################################
 
+# print information messages
 _print_info () {
   printf -- "info: %s\n" "$*"
 }
 
+# print debug messages (on sterr)
 _print_debug () {
   if [ "${DEBUG}" ]
   then
@@ -53,10 +62,12 @@ _print_debug () {
   fi
 }
 
+# print warning messages (on sterr)
 _print_warning () {
   printf -- "warning: %s\n" "$*" 1>&2
 }
 
+# print error messages (on sterr)
 _print_error () {
   printf -- "error: %s\n" "$*" 1>&2
 }
@@ -65,23 +76,60 @@ _print_error () {
 # main functions                                                     #
 ######################################################################
 
+# return usage/help information
+# if first argument is set, return extended usage.
 usage () {
-  printf -- "usage: %s [-s sleep] [-l fan level] [-c counter]\n" "$0"
+  note="  !!! Currently work only on FreeBSD !!!  "
+  flags="[-adhL]"
+  args="[-s second] [-l level] [-c level]"
+
+  a_msg="auto search thermal information in sysctl."
+  d_msg="debug mode, print debug information during execution."
+  h_msg="standard help, show simple usage."
+  H_msg="extended help, show extended usage with informations."
+  L_msg="loop mode, stop it with Ctrl-C or SIGKILL"
+
+  c_msg="(integer) fan level start, default 1."
+  l_msg="(integer) fan level, 0<level<7 for ibm fan, default 7."
+  s_msg="(integer) sleep time in second."
+
+  printf -- "usage: %s %s %s\n" "$0" "${flags}" "${args}"
+
+  if [ "${note}" ]
+  then
+    printf -- "%s\n" "${note}"
+  fi
+
+  if [ "$1" ]
+  then
+    printf -- "       -a: %s \n" "${a_msg}"
+    printf -- "       -d: %s \n" "${d_msg}"
+    printf -- "       -h: %s \n" "${h_msg}"
+    printf -- "       -H: %s \n" "${H_msg}"
+    printf -- "       -L: %s \n" "${L_msg}" 
+    printf -- "       -c: %s \n" "${c_msg}"
+    printf -- "       -l: %s \n" "${l_msg}"
+    printf -- "       -s: %s \n" "${s_msg}"
+  fi
   exit 1
 }
 
+# switch to automatic mode
 _automatic () {
   _print_info "automatic mode enabled"
   ret=$(sysctl dev.acpi_ibm.0.fan=1 2>&1)
   _print_debug "${ret}"
 }
 
+# switch to manual mode
 _manual () {
   _print_info "manual mode enabled"
   ret=$(sysctl dev.acpi_ibm.0.fan=0 2>&1)
   _print_debug "${ret}"
 }
 
+# switch to level
+# first argument set level. Only integer.
 _level () {
   level="$1"
   if echo "${level}" | egrep "^[0-9]+$" 2>&1 >/dev/null
@@ -94,17 +142,15 @@ _level () {
   fi
 }
 
+# return fan speed
 _speed () {
   sysctl -n dev.acpi_ibm.0.fan_speed
 }
 
+# return temperature and thermal informations
 _temp () {
-  sensors="hw.acpi.thermal.tz0.temperature
-           hw.acpi.thermal.tz1.temperature
-           dev.cpu.0.temperature
-           dev.cpu.1.temperature"
   buf=""
-  for sensor in ${sensors}
+  for sensor in ${SENSORS}
   do
     ret=$(sysctl -n "${sensor}")
     if [ "${ret}" ]
@@ -117,7 +163,59 @@ _temp () {
       fi
     fi
   done
- printf -- "%s" "${buf}"
+ if [ "${buf}" ]
+ then
+   printf -- "%s" "${buf}"
+ else
+   _print_debug "no sensors informations set"
+ fi
+}
+
+# search thermal information in sysctl
+_find_thermal () {
+  _print_debug "launch thermal information search..."
+  b=""
+  buf=""
+
+  for key in ${THERMAL_KEYS}
+  do
+    b=$(sysctl -a \
+             | egrep -re "^$key" \
+             | cut -d: -f1)
+    if [ "${buf}" ]
+    then
+      buf="${buf} ${b}"
+    else
+      buf="${b}"
+    fi
+    if [ "${b}" ]
+    then
+      _print_debug "thermal information found: ${b}"
+    fi
+  done
+
+  if [ "${buf}" ]
+  then
+    printf -- "%s" "${buf}"
+    return 0
+  else
+    _print_debug "no thermal information found!"
+    printf --
+    return 1
+  fi
+}
+
+# main function for check fan
+_main () {
+  while [ "${COUNTER}" -le "${FAN_LEVEL}" ]
+  do
+    _level "${COUNTER}"
+    _print_info "fan speed is: $(_speed)"
+    _print_info "temperature: $(_temp)"
+    sleep $SLEEP
+    COUNTER=$(($COUNTER+1))
+  done
+  return 0
 }
 
 ######################################################################
@@ -132,38 +230,60 @@ trap "_automatic; exit 1;"  SIGTERM
 trap "_level 0;"            SIGUSR1
 trap "_level ${FAN_LEVEL};" SIGUSR2
 
-args=$(getopt hds:c:l: $*)
+args=$(getopt hHdaLs:c:l: $*)
 set -- $args
 
 while true
 do
   case "$1" in
-    -h) usage;;
+    -h) usage; 
+        break;;
+    -H) usage extended
+        break;;
+
     -d) DEBUG="yes";
         _print_debug "set debug";
         shift;;
+
+    -L) LOOP_MODE="yes";
+        _print_debug "set loop_mode";
+        shift;;
+
+    -a) AUTO_KEYS="yes";
+        _print_debug "set auto_keys";
+        SENSORS=$(_find_thermal);
+        shift;;
+
     -s) SLEEP="$2";
         _print_debug "set sleep to $2";
         shift; shift;;
+
     -c) COUNTER="$2";
         _print_debug "set counter to $2";
         shift; shift;;
+
     -l) FAN_LEVEL="$2";
         _print_debug "set level to $2";
         shift; shift;;
+
     --) shift; break;;
   esac
 done
 
 _manual
 
-while [ "${COUNTER}" -le "${FAN_LEVEL}" ]
-do
-  _level "${COUNTER}"
-  _print_info "fan speed is: $(_speed)"
-  _print_info "temperature: $(_temp)"
-  sleep $SLEEP
-  COUNTER=$(($COUNTER+1))
-done
+if [ "${LOOP_MODE}" ]
+then
+  while _main
+  do 
+    sleep 1
+  COUNTER=0
+  done
+else
+  _main
+fi
+
+main
 
 _automatic
+
